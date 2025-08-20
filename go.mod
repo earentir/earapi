42247a3a80715b2e59292f0ecc9e60ad02b544df
earentir <97396839+earentir@users.noreply.github.com>
2025-08-20 06:14:26 -0700

    Add Cobra-based CLI flags

--- go.mod
+++ go.mod
@@ -1,7 +1,8 @@
 module earapi
 
-go 1.21.5
-toolchain go1.24.1
+go 1.23.0
+
+toolchain go1.24.3
 
 //replace github.com/earentir/netflixtudumscrapper => ../netflixtudumscrapper
 
@@ -14,6 +15,7 @@ require (
 	github.com/earentir/steamapidata v1.0.3
 	github.com/earentir/tmdbapidata v1.0.0
 	github.com/gin-gonic/gin v1.9.1
+	github.com/spf13/cobra v1.9.1
 )
 
 require (
@@ -29,6 +31,7 @@ require (
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.17.0 // indirect
 	github.com/goccy/go-json v0.10.2 // indirect
+	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.6 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
@@ -36,6 +39,7 @@ require (
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pelletier/go-toml/v2 v2.1.1 // indirect
+	github.com/spf13/pflag v1.0.6 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.12 // indirect
 	golang.org/x/arch v0.7.0 // indirect
--- main.go
+++ main.go
@@ -11,77 +11,99 @@ import (
 	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/spf13/cobra"
 )
 
 var (
 	apiversion = "v0.0.26"
-	configFile = "config/earapi.json"
+	configFile string
+	ip         string
+	port       string
 	config     earapiSettings
 )
 
-func main() {
+var rootCmd = &cobra.Command{
+	Use:   "earapi",
+	Short: "Start the earapi server",
+	Run: func(cmd *cobra.Command, args []string) {
+		loadConfig()
 
-	loadConfig()
+		if cmd.Flags().Changed("ip") {
+			config.API.IP = ip
+		} else if config.API.IP == "" {
+			config.API.IP = ip
+		}
 
-	//setup gin to build the API
-	r := gin.Default()
+		if cmd.Flags().Changed("port") {
+			config.API.Port = port
+		} else if config.API.Port == "" {
+			config.API.Port = port
+		}
 
-	// Handler for the root path
-	r.GET("/", func(c *gin.Context) { rootHandler(c, r) })
+		r := gin.Default()
 
-	steamv1Group := r.Group("/steam/v1/")
-	{
-		// steamGroup.GET("/", steamHandler)
-		steamv1Group.GET("/top", steamTopHandler)
-		steamv1Group.GET("/getuserid", steamUserIDHandler)
-		steamv1Group.GET("/appsused", steamUserAppsUsedHandler)
-		steamv1Group.GET("/appdata", steamAppDataHandler)
-		steamv1Group.GET("/search", searchSteamAppHandler)
-	}
+		// Handler for the root path
+		r.GET("/", func(c *gin.Context) { rootHandler(c, r) })
 
-	r.GET("/joke", jokeHandler)
+		steamv1Group := r.Group("/steam/v1/")
+		{
+			steamv1Group.GET("/top", steamTopHandler)
+			steamv1Group.GET("/getuserid", steamUserIDHandler)
+			steamv1Group.GET("/appsused", steamUserAppsUsedHandler)
+			steamv1Group.GET("/appdata", steamAppDataHandler)
+			steamv1Group.GET("/search", searchSteamAppHandler)
+		}
 
-	tmdbGroup := r.Group("/tmdb/v1/")
-	{
-		// movieGroup.GET("/", movieHandler)
-		tmdbGroup.GET("/search", movieSearchHandler)
-		// movieGroup.GET("/actor", movieActorHandler)
-	}
+		r.GET("/joke", jokeHandler)
 
-	netflixGroup := r.Group("/netflix/v1/")
-	{
-		netflixGroup.GET("/top", netflixTopHandler)
-	}
+		tmdbGroup := r.Group("/tmdb/v1/")
+		{
+			tmdbGroup.GET("/search", movieSearchHandler)
+		}
 
-	r.GET("/version", versionHandler)
+		netflixGroup := r.Group("/netflix/v1/")
+		{
+			netflixGroup.GET("/top", netflixTopHandler)
+		}
 
-	// r.Run(fmt.Sprintf("%s%s", ":", config.API.Port))
+		r.GET("/version", versionHandler)
 
-	httpserver :=
-		&http.Server{
-			Addr:    fmt.Sprintf("%s%s", ":", config.API.Port),
+		httpserver := &http.Server{
+			Addr:    fmt.Sprintf("%s:%s", config.API.IP, config.API.Port),
 			Handler: r,
 		}
 
-	go func() {
-		if err := httpserver.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+		go func() {
+			if err := httpserver.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+				fmt.Println(err)
+			}
+		}()
+
+		signals := make(chan os.Signal)
+		signal.Notify(signals, syscall.SIGINT, syscall.SIGTERM)
+
+		<-signals
+		fmt.Println("Shutting down the API")
+
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+
+		if err := httpserver.Shutdown(ctx); err != nil {
 			fmt.Println(err)
 		}
+	},
+}
 
-	}()
+func init() {
+	rootCmd.Flags().StringVar(&configFile, "config", "config/earapi.json", "Path to config file")
+	rootCmd.Flags().StringVar(&ip, "ip", "127.0.0.1", "IP address to listen on")
+	rootCmd.Flags().StringVar(&port, "port", "8080", "Port to listen on")
+}
 
-	//setup channels for capturing the termination signal from the OS
-	signals := make(chan os.Signal)
-	signal.Notify(signals, syscall.SIGINT, syscall.SIGTERM)
-
-	<-signals
-	fmt.Println("Shutting down the API")
-
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
-
-	if err := httpserver.Shutdown(ctx); err != nil {
+func main() {
+	if err := rootCmd.Execute(); err != nil {
 		fmt.Println(err)
+		os.Exit(1)
 	}
 }
 
